a378f630ed364c31fa1e2f3db19e6b4122261fd9
salonis <salonishah@berkeley.edu>
2015-09-08 02:32:23 -0700

    redirect after subsubmission of form to confirmation page, which ends with unique id. if returning to url with confirmation id, the confirmation details for that appt will show. temporarily using array as db

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+
+appointments = []
 
 @app.route("/", methods=["GET"])
 def hello():
@@ -8,8 +11,16 @@ def hello():
 
 @app.route("/", methods=["POST"])
 def make_appointment():
-	
+	appointments.append((request.form['name'], request.form['email'], request.form['nailtype']))
 	print request.form['name'], request.form['email'], request.form['nailtype']
+	return redirect("confirmation/%d/" % (len(appointments) - 1), code=302)
+
+@app.route("/confirmation/<int:id>/", methods=["GET"])
+def confirmation(id):
+	appt = appointments[id]
+	return render_template('confirmation.tpl', 
+		name=appt[0], email=appt[1], nailtype=appt[2])
+
 # @app.route("/person/<string:name>" methods=["GET"])
 # def person(name):
 # 	return render_template('hello.tpl', name=name)
@@ -17,4 +28,4 @@ def make_appointment():
 # def post_person(name):
 # 	return render_template('hello.tpl', name=name)
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ templates/confirmation.tpl
@@ -0,0 +1,6 @@
+<!doctype html>
+<html>
+	<div id="thanks">Thanks for booking an appointment!</div>
+
+	<div id="confirm">Your appointment is booked for {{name}} at {{nailtype}}
+</html>
