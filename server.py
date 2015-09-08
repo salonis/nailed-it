df7e24d0a1c73a15f56f97c2f95392bcc5e180d6
salonis <salonishah@berkeley.edu>
2015-09-08 01:29:34 -0700

    make booking form with name, email, desired nail design as inputs. form post req.

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
 	return render_template('hello.tpl')
 
-@app.route("/person/<string:name>" methods=["GET"])
-def person(name):
-	return render_template('hello.tpl', name=name)
-@app.route("/person/<string:name>", methods=["POST"])
-def post_person(name):
-	return render_template('hello.tpl', name=name)
+
+@app.route("/", methods=["POST"])
+def make_appointment():
+	
+	print request.form['name'], request.form['email'], request.form['nailtype']
+# @app.route("/person/<string:name>" methods=["GET"])
+# def person(name):
+# 	return render_template('hello.tpl', name=name)
+# @app.route("/person/<string:name>", methods=["POST"])
+# def post_person(name):
+# 	return render_template('hello.tpl', name=name)
 if __name__ == "__main__":
     app.run()
--- templates/hello.tpl
+++ templates/hello.tpl
@@ -1,7 +1,24 @@
 <!doctype html>
-<title>Hello from Flask</title>
-{% if name %}
+<html>
+<title>Nailed It</title>
+<!-- {% if name %}
   <h1>Hello {{ name }}!</h1>
 {% else %}
   <h1>Hello World!</h1>
-{% endif %}
+{% endif %} -->
+	<form method="POST">
+		Name: 
+		<input type="text" name="name">
+		<br>
+		Email (to contact you for confirmation and scheduling issues): 
+		<input type="text" name="email">
+		<br>
+		<input type="radio" name="nailtype" value="plain" checked>Solid nails ($10)
+		<br>
+		<input type="radio" name="nailtype" value="dots">Polka dots ($15)
+		<br>
+		<input type="radio" name="nailtype" value="ombre">Ombre gradient nails ($25)
+		<br><br>
+		<input type="submit" value="Submit">
+	</form>
+</html>
